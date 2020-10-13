a29276a974f8627c1ff4046a266fcd72ef3d993c
pgoytia <pgoytia@gmail.com>
2020-10-13 15:16:06 -0500

    wowow2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 5000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, '/index.html'));
 });app.listen(port, () => {
    console.log('Server is up!');
 });
