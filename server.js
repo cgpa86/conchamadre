706ded59749d5b0b671ff3347894d76acbd9c75e
pgoytia <pgoytia@gmail.com>
2020-10-15 01:52:28 -0500

    procfile server runbuild2

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const colors = ('colors');
 
 const publicPath = path.join(__dirname,'public');
 const port = process.env.PORT || 5000;
-app.use(express.static(publicPath));app.get('*', (req, res) => {
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });app.listen(port, () => {
    console.log('Server is Arriba!');
