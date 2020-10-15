721317f27ecfcdda301d18e1c1f91b9a72c74d04
pgoytia <pgoytia@gmail.com>
2020-10-15 01:32:44 -0500

    procfile

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const colors = ('colors');
+
+const publicPath = path.join(__dirname,'public');
+const port = process.env.PORT || 5000;
+app.use(express.static(publicPath));app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});app.listen(port, () => {
+   console.log('Server is Arriba!');
+   console.log(__dirname);
+});
