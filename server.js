643edc32ed9dc777e8474d6d85ba21c93513d9d2
pgoytia <pgoytia@gmail.com>
2020-10-15 02:53:34 -0500

    againx2222

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const colors = ('colors');
 //algo
-const publicPath = path.join(__dirname,'...', 'public');
+const publicPath = path.join(__dirname,'public');
 const port = process.env.PORT || 5000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
